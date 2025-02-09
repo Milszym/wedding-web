2d01ee8ec17c3b621d8698d6f9a2da93f8a006f4
Szymon Miloch <szymon.miloch@brightinventions.pl>
2025-02-09 14:15:52 +0100

    Fix translations for iOS

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,15 +1,25 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
-import Backend from "i18next-http-backend"; // Load translations from a server
 import LanguageDetector from "i18next-browser-languagedetector"; // Detect user language
+import translation_en from "./locales/en/translation.json";
+import translation_pl from "./locales/pl/translation.json";
+
+const resources = {
+  en: {
+      translation: translation_en
+  },
+  pl: {
+    translation: translation_pl
+  },
+};
 
 i18n
-  .use(Backend) // Load translation files
-  .use(LanguageDetector) // Detect browser language
+  .use(LanguageDetector) // Bind react-i18next
   .use(initReactI18next) // Bind react-i18next
   .init({
+    resources,
     fallbackLng: "pl", // Default language
-    debug: true, // Set to false in production
+    defaultNS: 'translation',
     interpolation: {
       escapeValue: false, // React already escapes by default
     },
